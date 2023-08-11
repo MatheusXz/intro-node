81f9411c03b166fc3c496138a402c8fce7d64865
MatheusXz <matheus000000018@gmail.com>
2023-08-11 20:30:52 -0300

    up

--- database/connection.js
+++ database/connection.js
@@ -1,15 +1,27 @@
 const mysql = require('mysql2/promise');
 
 const bd_user = 'ewerton_node';
+const bd_port = 3306;
 const bd_password = '123456';
 const bd_host = '10.67.22.216';
-const bd_port = 3306;
 const bd_database = 'ewerton_node';
 
-const connection = mysql.createConnection({
-    user: bd_user,
-    password: bd_password,
+const config = {
     host: bd_host,
     port: bd_port,
-    database:bd_database,
-})
+    user: bd_user,
+    password: bd_password,
+    database: bd_database,
+    waitForConnections: true,
+    connectTimeout: 10,
+    queueLimit: 0,
+}
+
+try {
+    connection = mysql.createPool(config);
+    console.log("Chamou conexão MySql!");
+} catch {
+    console.log(error);
+}
+
+module.exports = connection;
